82c70674fed36cbd9a8a8a5816e19cec18da36a2
Najtmare <millan@sino.net>
2014-07-21 16:46:09 +0200

    fix nav and before_action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
