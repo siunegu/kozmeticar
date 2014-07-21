31c81ea2315ae0e76bfbe03e2df1bfaea3f67e43
Najtmare <millan@sino.net>
2014-07-21 17:09:21 +0200

    add a search form

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,4 +19,11 @@ class Product < ActiveRecord::Base
   validates :description, :presence => true
   validates :employee, :presence => true
 
+  def self.search_and_order(search)
+    if search
+      where("name LIKE ?", "%#{search.downcase}%").order(
+        is_promoted: :desc, name: asc).upcoming
+    end
+  end
+
 end
