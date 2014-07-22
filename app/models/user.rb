a136dec40bcbee3d50f354520a2fc99c986b7613
Dalton Dick <daltondick2010@gmail.com>
2014-07-21 20:26:18 -0500

    Different way of setting admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,22 @@
 class User < ActiveRecord::Base
 
   has_paper_trail
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  after_create :ensure_at_least_one_admin
+  before_create :ensure_at_least_one_admin
 
   has_many :products
   has_many :reservations
 
   # By default this will make sure the first user to registers gets admin status
   def ensure_at_least_one_admin
-  	if User.count == 1
-  		u = User.first
-  		u.admin = true
-  		u.save!
-  	end
+    unless User.any?
+      self.admin = true
+    end
   end
 
 end
