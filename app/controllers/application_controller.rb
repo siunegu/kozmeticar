ee66693ccb6a129c3687e65e80d62f9b60e41718
Najtmare <millan@sino.net>
2014-08-13 00:04:11 +0200

    Revert "fix"
    This reverts commit d03c36d90793b3bf4b3899dc4eaa89cdec8bed45.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,13 +11,17 @@ class ApplicationController < ActionController::Base
   	authenticate_user!
 
   	if current_user && !current_user.admin?
-  		redirect_to root_path
+  		render_404
   	end
   end
   helper_method :require_admin!
 
   def peek_enabled?
     current_user.try(:admin?)
+  end
+
+  def render_404
+    render: file: Rails.root.join("public", "404"), layout: false, status: 404
   end
 
   protected
